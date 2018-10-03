6fbc89e7816e1821af5d3a5eadb00bd1f8257b98
Aceeri <conmcclusk@gmail.com>
2018-10-03 01:37:14 -0700

    Parse command, loop on interval

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,14 @@
 
-#[derive(Debug, Clone)]
+use std::io;
+
+#[derive(Debug)]
 pub enum RatchError {
-    //IoError(io::Error),
+    IoError(io::Error),
     ParseError(String),
+}
+
+impl From<io::Error> for RatchError {
+    fn from(error: io::Error) -> Self {
+        RatchError::IoError(error)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,14 @@ extern crate clap;
 
 mod error;
 
-use self::error::RatchError;
-use clap::{App, Arg, SubCommand};
-//use std::error::Error;
+use std::io::{Write, Read, stdout};
+use std::process::{Command, Stdio};
+use std::thread;
+use std::time::Duration;
 
-const DEFAULT_INTERVAL: f64 = 2.0;
+use self::error::RatchError;
+use clap::{App, AppSettings, Arg, SubCommand};
+//use std::error::Error;
 
 fn parse_interval(interval: Option<&str>) -> Result<f64, RatchError> {
     match interval {
@@ -17,7 +20,9 @@ fn parse_interval(interval: Option<&str>) -> Result<f64, RatchError> {
                 value
             ))),
         },
-        None => Ok(DEFAULT_INTERVAL),
+        None => Err(RatchError::ParseError(
+            "Interval value not found".to_owned(),
+        )),
     }
 }
 
@@ -25,16 +30,60 @@ fn main() -> Result<(), RatchError> {
     let matches = App::with_defaults("My Program")
         .author("Aceeri <conmcclusk@gmail.com>")
         .about("A better `watch`")
+        .setting(AppSettings::TrailingVarArg)
         .arg(
             Arg::with_name("interval")
                 .short("n")
                 .long("interval")
                 .takes_value(true)
+                .default_value("2.0")
                 .help("Interval to update the program"),
-        ).get_matches();
+        ).arg(Arg::with_name("command").required(true).multiple(true))
+        .get_matches();
 
     let interval = parse_interval(matches.value_of("interval"))?;
     println!("Interval: {:?}", interval);
+
+    let command = match matches.values_of("command") {
+        Some(command) => command.collect::<Vec<&str>>(),
+        None => return Err(RatchError::ParseError("command not found".to_string())),
+    };
+
+    println!("Command: {:?}", command.clone());
+    let mut buffer: Vec<u8> = Vec::new();
+    loop {
+        buffer.clear();
+
+        let spawn_result = Command::new("sh")
+            .arg("-c")
+            .args(command.clone())
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn();
+
+        let mut child = spawn_result?;
+        let done = child.wait()?;
+        match done.code() {
+            Some(code) => println!("Process exitted with status code: {}", code),
+            None => println!("Process terminated by signal"),
+        }
+
+        if let Some(ref mut stdout) = child.stdout {
+            stdout.read_to_end(&mut buffer);
+        }
+
+        if let Some(ref mut stderr) = child.stderr {
+            stderr.read_to_end(&mut buffer);
+        }
+
+        //println!("buffer:");
+        println!("buffer: {:?}", buffer);
+        //print!("`");
+        stdout().write(&buffer);
+        //print!("`");
+
+        thread::sleep(Duration::from_nanos((interval * 1_000_000_000.0) as u64));
+    }
 
     Ok(())
 }
